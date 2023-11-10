dfc157990003c45ff7c264aa43a2e70cab34fc12
devsekret <eshboltayevobekjon@gmail.com>
2023-11-10 20:38:27 +0500

    ma'noli_savollar

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var username = String (prompt("Ismingizni hamma yaxshi biladi", "Buronbek"))
+console.log(username)
+
+var yosh = Number (prompt("Yoshingizni ham yaxshi bilamiz", "21"))
+console.log (yosh)
+
+var savol_1 = String(prompt("Qachon yangi guruh bilan shug'ullanasiz" , "Storm academy haqida"))
+console.log (savol_1)
+
+var savol_2 = String (prompt("O'zi guruhni tashkil qilish niyatingiz bormi"))
+console.log (savol_2)
+
+var savol_3 = String(prompt("O'zi buni ochishdan maqsad nima edi"))
+console.log (savol_3)
+
+var savol_4 = String(prompt("Nega uni storm acedemy deb nomladingiz"))
+console.log (savol_4)
+
+var savol_5 = String(prompt("Suhbat qachon bo'ladi"))
+console.log (savol_5)
+
+var savol_6 = String(prompt("Bizga guruhning o'zi nima foydasi bor"))
+console.log (savol_6)
+
+var savol_7 = String(prompt("Aksincha, Sizga guruhning nima foydasi bor"))
+console.log (savol_7)
+
+var savol_8= String(prompt("Biz guruhdan pul topishimiz mumkinmi" , "Taxminan qancha"))
+console.log (savol_8)
+
+
+var javob = [
+    "Ismingiz: " + " " + username + "\n" +
+    "Yoshingiz: " + " " + yosh + "\n" +
+    "Storm academy haqida : " + " " + savol_1 + "\n" +
+    "O'zi guruhni tashkil qilish niyatingiz bormi : " + " " + savol_2 + "\n" +
+    "O'zi buni ochishdan maqsad nima edi : " + " " + savol_3 + "\n" +
+    "Nega uni storm acedemy deb nomladingiz : " + " " + savol_4 + "\n" +
+    "Suhbat qachon bo'ladi : " + " " + savol_5 + "\n" +
+    "Bizga guruhning o'zi nima foydasi bor : " + " " + savol_6 + "\n" +
+    "Aksincha, Sizga guruhning nima foydasi bor : " + " " + savol_7 + "\n" +
+    "Biz guruhdan pul topishimiz mumkinmi : " + " " + savol_8 + "\n"
+]
+
+alert(javob)
+
